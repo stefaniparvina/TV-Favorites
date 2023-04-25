120aa444324f11162ad593743c816bb8fe497559
stefaniparvina <stefani.parvina@gmail.com>
2023-04-25 22:13:22 +0300

    DB scripts

--- dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[UserInfo]
-(
-	[email] VARCHAR(MAX) NOT NULL primary key, 
-    [firstName] VARCHAR(MAX) NOT NULL, 
-    [lastName] VARCHAR(MAX) NOT NULL, 
-    [password] VARCHAR(MAX) NOT NULL, 
-    [birthday] DATE NULL,
-    [id] int IDENTITY(1,1) NOT NULL
-)
--- dbo.WatchList.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[WatchList]
-(
-	[id] INT IDENTITY(1,1) PRIMARY KEY, 
-    [userId] INT FOREIGN KEY REFERENCES [User](id) NOT NULL, 
-    series INT INDEX IX_WatchList_Series NOT NULL
-)
